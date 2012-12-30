051ae7a0abf0b651f3c58544c34aafc3bbbf75bf
bsmith3541 <brandon.smith@yale.edu>
2012-12-30 05:30:01 -0600

    changed eid to bigint

---
+++ db/migrate/20121230112758_change_data_type_for_eid.rb
@@ -0,0 +1,13 @@
+class ChangeDataTypeForEid < ActiveRecord::Migration
+  def up
+  	change_table :events do |t|
+  		t.change :eid, :bigint
+  	end
+  end
+
+  def down
+  	change_table :events do |t|
+  		t.change :eid, :integer
+  	end
+  end
+end
